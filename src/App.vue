0489198873b96969c515f445b2236d3af4dcb238
Michiel <Michieldesmet1994@gmail.com>
2020-08-20 10:02:16 +0200

    New employees can now be saved to the application

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-form @add:employee="addEmployee"/>
+    <employee-table :employees="employees"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from "@/components/EmployeeTable";
+import EmployeeForm from "@/components/EmployeeForm";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeeForm,
+    EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        }, {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        }, {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        }, {
+          id: 4,
+          name: 'Bob de Bouwer',
+          email: 'bob.de.bouwer@bouwers.com',
+        },
+      ]
+    }
+  },
+  methods: {
+    addEmployee(employee) {
+      // This is to generate an ID
+      const lastId = this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id};
+      // Add new employee to the array list
+      this.employees = [...this.employees, newEmployee];
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
 }
 </style>
